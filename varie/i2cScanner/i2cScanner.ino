3e4caf1ea91dda386a52b6fc042f2c45c0c6d410
Paolo Aliverti <paolo.aliverti@gmail.com>
2024-10-21 19:04:11 +0200

    .

--- varie/i2cScanner/i2cScanner.ino
+++ varie/i2cScanner/i2cScanner.ino
@@ -34,7 +34,7 @@ void setup()
 {
   Wire.begin();
 
-  Serial.begin(9600);
+  Serial.begin(115200);
   while (!Serial);             // Leonardo: wait for serial monitor
   Serial.println("\nI2C Scanner");
 }
