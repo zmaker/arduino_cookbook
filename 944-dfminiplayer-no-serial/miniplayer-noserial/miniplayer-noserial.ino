5a8b54cc0d03c67245400a8af50f4eb11e4ce6aa
Paolo Aliverti <paolo.aliverti@gmail.com>
2025-11-10 09:33:55 +0100

    dfminiplayer

---
+++ 944-dfminiplayer-no-serial/miniplayer-noserial/miniplayer-noserial.ino
@@ -0,0 +1,15 @@
+void setup() {
+  pinMode(12, OUTPUT);
+  digitalWrite(12, HIGH);
+  delay(3000);
+
+  digitalWrite(12, LOW);
+  delay(200);
+  digitalWrite(12, HIGH);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
